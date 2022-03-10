4025444afda3512c410c4b3e69a4a9bdc9c93a00
Canleskis <91223490+Canleskis@users.noreply.github.com>
2022-03-10 03:35:26 +0100

    cleanup

--- start_webserver.bat
+++ start_webserver.bat
@@ -1,8 +1,8 @@
 cargo build --release --target wasm32-unknown-unknown
 
 set mypath=%cd%
-cd C:\Dev\rust\pong-clone\target\wasm32-unknown-unknown\release
-copy pong-clone.wasm %mypath%\docs
+cd target\wasm32-unknown-unknown\release
+copy pong_clone.wasm %mypath%\docs
 
 cd %mypath%/docs
 
